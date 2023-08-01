47744c0a75dc252c64d224748fc0299203e6793e
Dirk <dirkscgm@gmail.com>
2023-08-01 13:07:52 +0200

    Fixed Auth0 callback routine

--- app/views.py
+++ app/views.py
@@ -18,19 +18,14 @@ def login_view(request):
     client_id = settings.SOCIAL_AUTH_AUTH0_KEY
     redirect_uri = quote_plus(settings.SOCIAL_AUTH_AUTH0_CALLBACK_URL)
     return redirect(
-        f"https://{domain}/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}"
+        f"https://{domain}/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope=openid%20profile%20email%20offline_access%20read:current_user%20update:current_user_metadata"
     )
 
 
 @login_required
 def logout_view(request):
     auth_logout(request)
-    domain = settings.SOCIAL_AUTH_AUTH0_DOMAIN
-    client_id = settings.SOCIAL_AUTH_AUTH0_KEY
-    return_to = "http://127.0.0.1:8000"  # this can be current domain
-    return redirect(
-        f"https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}"
-    )
+    return redirect("/")
 
 
 def auth0_callback(request):
@@ -44,16 +39,15 @@ def auth0_callback(request):
         settings.SOCIAL_AUTH_AUTH0_SECRET,
     )
     auth0_users = Users(settings.SOCIAL_AUTH_AUTH0_DOMAIN)
-    token = get_token.authorization_code(code, "http://localhost:8000/auth0_callback")
+    token = get_token.authorization_code(code, settings.SOCIAL_AUTH_AUTH0_CALLBACK_URL)
     user_info = auth0_users.userinfo(token["access_token"])
 
     user, created = Auth0User.objects.get_or_create(auth0_id=user_info["sub"])
     if created:
         user.username = user_info["nickname"]
         user.email = user_info["email"]
-        user.auth0_id = user_info["sub"]
         user.save()
 
     # Log the user in and redirect to the desired page
-    login(request, user)
+    login(request, user, backend="django.contrib.auth.backends.ModelBackend")
     return redirect("/")
